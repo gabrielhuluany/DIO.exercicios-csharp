5f9f1f82c125a469300a11b646cb7e9adece8cb8
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-02-04 17:02:29 -0300

    Create Vai_Ter_Copa.cs

---
+++ Bootcamps/GFT Start #3 .NET/Vai Ter Copa/Vai_Ter_Copa.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+          string str;
+          while ((str=Console.ReadLine())!= null)
+          {
+            int x = int.Parse(str);
+            
+            if (x == 0)
+            {
+                Console.WriteLine("vai ter copa!");
+            }
+            else
+            {
+                Console.WriteLine("vai ter duas!");
+            }
+          }
+        }
+    }
+}
