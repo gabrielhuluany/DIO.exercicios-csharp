bbd533f555335f6e382f0a36f1ad025e9b931f86
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 09:23:10 -0300

    Update main.cs

--- Bootcamps/GFT Start #4 .NET/Quadrado e ao cubo/main.cs
+++ Bootcamps/GFT Start #4 .NET/Quadrado e ao cubo/main.cs
@@ -8,8 +8,7 @@ namespace DIO
         {
           int n = int.Parse(Console.ReadLine());
 
-          for(int i = 1; i <= n; i++)
-          {
+          for(int i = 1; i <= n; i++) {
              Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3) + "\n");
           }
           Console.ReadKey();
