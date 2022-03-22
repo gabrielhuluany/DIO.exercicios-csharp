0fa242dfd9b0cc8240e809177af2b05f16fdf056
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 09:11:33 -0300

    Update main.cs

--- Bootcamps/GFT Start #3 .NET/Media 3/main.cs
+++ Bootcamps/GFT Start #3 .NET/Media 3/main.cs
@@ -16,32 +16,27 @@ namespace DIO
           avg = (((a * 2) + (b * 3) + (c * 4) + (d * 1)) / 10);
           Console.WriteLine("Media: {0:F1}", avg);
           
-          if (avg >= 7.0)
-          {
+          if(avg >= 7.0) {
               Console.WriteLine("Aluno aprovado.");
           }
           
-          if (avg < 5.0)
-          {
+          if(avg < 5.0) {
               Console.WriteLine("Aluno reprovado.");
           }
           
-          if (avg >= 5.0 && avg <= 6.9)
-          {
+          if(avg >= 5.0 && avg <= 6.9) {
             Console.WriteLine("Aluno em exame.");
             e = double.Parse(Console.ReadLine());
             Console.WriteLine("Nota do exame: {0:F1}", e);
             avg = (e + avg) / 2;
             
-            if (avg >= 5.0)
-            {
+            if(avg >= 5.0) {
                 Console.WriteLine("Aluno aprovado.");
             }
             
-            if (avg < 5.0)
-            {
+            if(avg < 5.0) {
                 Console.WriteLine("Aluno reprovado");
-                //mensagem sem o ponto final
+                //erro: mensagem sem o ponto final!!
             }
             
             Console.WriteLine("Media final: {0:F1}", avg);
