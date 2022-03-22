955bc75b7648e7e149cf20e3553185ab4f1be8a6
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 08:15:18 -0300

    Create main.cs

---
+++ Bootcamps/GFT Start #4 .NET/Quadrado e ao cubo/main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+
+          for(int i = 1; i <= n; i++)
+          {
+             Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3) + "\n");
+          }
+        }
+    }
+}
