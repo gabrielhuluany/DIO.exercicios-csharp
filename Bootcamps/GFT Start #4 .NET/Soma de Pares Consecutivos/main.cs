05c8a6c742bb5cf584f46d45c929b8e91a9b18bb
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 08:20:46 -0300

    Create main.cs

---
+++ Bootcamps/GFT Start #4 .NET/Soma de Pares Consecutivos/main.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int D = 1;
+          
+          while(D != 0) {
+            D = int.Parse(Console.ReadLine());
+            
+            if(D == 0){
+              break;
+            }
+            
+            if(D % 2 == 0){
+              Console.WriteLine(D + (D + 2) + (D + 4) + (D + 6) + (D + 8));
+            } else {
+              Console.WriteLine((D + 1) + (D + 3) + (D + 5) + (D + 7) + (D + 9));
+            }
+          }
+        }
+    }
+}
