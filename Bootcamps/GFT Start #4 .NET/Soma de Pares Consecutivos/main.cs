c72642192c266ce33e38d55057405b8b0071b564
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 09:24:09 -0300

    Update main.cs

--- Bootcamps/GFT Start #4 .NET/Soma de Pares Consecutivos/main.cs
+++ Bootcamps/GFT Start #4 .NET/Soma de Pares Consecutivos/main.cs
@@ -11,11 +11,11 @@ namespace DIO
           while(D != 0) {
             D = int.Parse(Console.ReadLine());
             
-            if(D == 0){
+            if(D == 0) {
               break;
             }
             
-            if(D % 2 == 0){
+            if(D % 2 == 0) {
               Console.WriteLine(D + (D + 2) + (D + 4) + (D + 6) + (D + 8));
             } else {
               Console.WriteLine((D + 1) + (D + 3) + (D + 5) + (D + 7) + (D + 9));
