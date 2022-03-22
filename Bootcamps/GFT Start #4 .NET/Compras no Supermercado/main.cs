34861df6ab307909765dcb8183e6f0a46cf41b59
Gabriel Huluany <gabrielhuluany@outlook.com>
2022-03-22 08:50:48 -0300

    Create main.cs

---
+++ Bootcamps/GFT Start #4 .NET/Compras no Supermercado/main.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+          
+          for(int i = 0; i < n; i++) {
+            List<string> lista = new List<String>(Console.ReadLine().Split(' '));
+            lista = lista.Distinct().ToList();
+            lista.Sort();
+
+            foreach(string item in lista) {
+              Console.Write($"{item} ");
+            }
+            
+            Console.WriteLine(" ");
+          }
+          Console.ReadKey();
+        }
+    }
+}
